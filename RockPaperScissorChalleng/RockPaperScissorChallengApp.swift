0c6e4c2a4e51f0316bd2e3455277c1de2da31050
Omolemo Mashigo <omomashigo@gmail.com>
2025-02-10 17:56:50 +0200

    Initial Commit

---
+++ RockPaperScissorChalleng/RockPaperScissorChallengApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorChallengApp.swift
+//  RockPaperScissorChalleng
+//
+//  Created by Omolemo Mashigo on 2025/02/10.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorChallengApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
