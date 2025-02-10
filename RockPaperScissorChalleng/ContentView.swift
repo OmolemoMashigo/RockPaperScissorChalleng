60b1681fc5a0f0c3619207d0c9288de5136fd844
Omolemo Mashigo <omomashigo@gmail.com>
2025-02-10 19:09:43 +0200

    initial properties

--- RockPaperScissorChalleng/ContentView.swift
+++ RockPaperScissorChalleng/ContentView.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private let moves = ["Rock", "Paper", "Scissor"]
+    
+    @State private var appChoice = Int.random(in: 0...2)
+    @State private var win = Bool.random()
+    @State private var score = 0
+    @State private var questionCount = 0
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
