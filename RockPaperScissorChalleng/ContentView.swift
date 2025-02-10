6b596e0ac936a80e36910d26e4aa313dc433fe84
Omolemo Mashigo <omomashigo@gmail.com>
2025-02-10 21:56:01 +0200

    logic for determining scores

--- RockPaperScissorChalleng/ContentView.swift
+++ RockPaperScissorChalleng/ContentView.swift
@@ -12,9 +12,10 @@ struct ContentView: View {
     private let moves = ["Rock", "Paper", "Scissor"]
     
     @State private var appChoice = Int.random(in: 0...2)
-    @State private var win = Bool.random()
+    @State private var mustWin = Bool.random()
     @State private var score = 0
     @State private var questionCount = 0
+    @State private var gameOverAlert = true
     
     var body: some View {
         VStack(spacing: 30) {
@@ -24,12 +25,47 @@ struct ContentView: View {
             Text(moves[appChoice])
                 .font(.system(size: 50))
             
-            Text(win ? "WIN": "LOSE")
+            Text(mustWin ? "WIN": "LOSE")
                 .font(.title2)
-                .foregroundColor(win ? .green : .red)
+                .foregroundColor(mustWin ? .green : .red)
             
+            HStack(spacing: 20){
+                ForEach(0..<3){ index in
+                    Button(moves[index]){
+                        moveTapped(index)
+                    }
+                }
+            }
         }
         .padding()
+        
+        
+    }
+    
+    func moveTapped(_ moveIndex: Int){
+       
+        let correctMoveIndex: Int
+                
+                if mustWin {
+                    correctMoveIndex = (appChoice + 1) % 3
+                } else {
+                    correctMoveIndex = (appChoice + 2) % 3
+                }
+                
+                if moveIndex == correctMoveIndex {
+                    score += 1
+                } else {
+                    score -= 1
+                }
+                
+                questionCount += 1
+                
+                if questionCount == 10 {
+                    gameOverAlert = true
+                } else {
+                    appChoice = Int.random(in: 0...2)
+                    mustWin.toggle()
+                }
     }
 }
 
